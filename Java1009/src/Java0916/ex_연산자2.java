308f26b6151f08ca4af53f46a7779489abf67d96
smhrd <smhrd@DESKTOP-Q1AI6AJ>
2021-10-12 17:36:29 +0900

    연산자2

---
+++ Java1009/src/Java0916/ex_연산자2.java
@@ -0,0 +1,37 @@
+package Java0916;
+
+public class ex_연산자2 {
+
+	public static void main(String[] args) {
+		// 정수 , 정수 -> 정수
+				// 실수, 실수 -> 실수
+				// 정수, 실수 -> 실수
+				
+				double num1 = 13.2;
+				double num2 = 2;
+				
+				//System.out.println(num1 / num2);
+				//System.out.println(num1 % num2);
+				
+				int num3 = 10;
+				int num4 = 7;
+				
+				int num5 = 10;
+				String num6 = "7";
+				
+				System.out.println(num5 + num6);
+				
+				
+				String a = 10 + 7 + "";
+				String b = 8 + "" + 2;
+				String c = "" + 5 + 2;
+				
+				System.out.println(a);
+				System.out.println(b);
+				System.out.println(c);
+				
+				
+
+	}
+
+}
